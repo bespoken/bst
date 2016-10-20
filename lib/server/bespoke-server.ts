2083216ef314f7c3ec70af1a0b87ec4c87529fa7
John Kelvie <john.kelvie@gmail.com>
2016-10-20 15:20:26 -0500

    Small updated

--- lib/server/bespoke-server.ts
+++ lib/server/bespoke-server.ts
@@ -17,8 +17,7 @@ export class BespokeServer {
     public start (started?: () => void): void {
         let self = this;
         console.error("AWS_KEY: " + process.env["AWS_ACCESS_KEY_ID"]);
-        console.error("AWA_KEY: " + process.env["AWS_SECRET_ACCESS_KEY"]);
-
+        
         let count = 0;
         // Make sure both NodeManager and WebhookManager have started
         let callbackCounter = function () {
