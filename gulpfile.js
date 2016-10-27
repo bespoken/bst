17c78b139f3bf10dd675b45bd61c3177673228be
John Kelvie <john.kelvie@gmail.com>
2016-10-27 18:47:53 -0500

    Updated docs

--- gulpfile.js
+++ gulpfile.js
@@ -36,13 +36,7 @@ gulp.task('docs', ['mkdocs', 'typedoc']);
 
 gulp.task('mkdocs', function() {
     // We don't want to add css tags to the main README - replace them with blank
-    gulp.src('docs/README.raw.md')
-        .pipe(replace('{{badge.css}}', ''))
-        .pipe(rename('README.md'))
-        .pipe(gulp.dest('.'));
-
-    gulp.src('docs/README.raw.md')
-        .pipe(replace('{{badge.css}}', '{: class="badge" }'))
+    gulp.src('README.md')
         .pipe(rename('index.md'))
         .pipe(gulp.dest('docs/'));
 });
