04608eeb2590bd60c897508b773a554d31d8b2a4
John Kelvie <john.kelvie@gmail.com>
2016-12-16 15:29:08 -0500

    Updated definitions

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 "use strict";
+var audio_item_1 = require("./lib/alexa/audio-item");
+exports.AudioItem = audio_item_1.AudioItem;
+var audio_item_2 = require("./lib/alexa/audio-item");
+exports.AudioItemStream = audio_item_2.AudioItemStream;
 var bst_alexa_1 = require("./lib/client/bst-alexa");
 exports.BSTAlexa = bst_alexa_1.BSTAlexa;
+var bst_alexa_2 = require("./lib/client/bst-alexa");
+exports.BSTAlexaEvents = bst_alexa_2.BSTAlexaEvents;
 var bst_encode_1 = require("./lib/client/bst-encode");
 exports.BSTEncode = bst_encode_1.BSTEncode;
-var logless_1 = require("./lib/logless/logless");
-exports.Logless = logless_1.Logless;
 var lambda_server_1 = require("./lib/client/lambda-server");
 exports.LambdaServer = lambda_server_1.LambdaServer;
+var logless_1 = require("./lib/logless/logless");
+exports.Logless = logless_1.Logless;
 //# sourceMappingURL=index.js.map
