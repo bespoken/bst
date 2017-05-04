633f7d10b6a4d6a6053424bb00bc3ac84ad5e9ce
John Kelvie <john.kelvie@gmail.com>
2017-05-03 20:08:12 -0500

    Updated test

--- test/client/bst-proxy-test.ts
+++ test/client/bst-proxy-test.ts
@@ -101,8 +101,7 @@ describe("BSTProxy", async function() {
     describe("#urlgen()", function() {
         it("Starts and Stops Correctly", function (done) {
             let url = BSTProxy.urlgen("http://jpk.com/test");
-            assert.equal(url, "https://bespoken.tools/dashboard/test?id="
-                + Global.config().sourceID() + "&key=" + Global.config().secretKey());
+            assert.equal(url, "https://proxy.bespoken.tools/test?node-id=" + Global.config().secretKey());
             done();
         });
     });
