95c31148ca8f9898edf7e167bc7f1c5b9c19c618
Edgar Cruzado <edgarivan.cruzado@gmail.com>
2018-08-28 17:14:14 -0500

    Warn on no using last version (#486)
    * warn user on not using last version
    
    * display custom messages

--- bin/bst-test.ts
+++ bin/bst-test.ts
@@ -8,7 +8,6 @@ const skillTesting = require("skill-testing-ml");
 program.version(Global.version());
 
 Global.initializeCLI(false).then(() => {
-
     program
         .usage("[test-pattern-regex]")
         .description("Runs unit-tests for a skill - automatically searches for YML test files and runs them");
