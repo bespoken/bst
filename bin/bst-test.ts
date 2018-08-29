9210bac7a725265fc83e65be65e9ab2ff6239b81
Edgar Cruzado <edgarivan.cruzado@gmail.com>
2018-08-29 10:20:29 -0500

    order options on test, fix default parameter (#488)
    * order options on test, default parameter
    
    * remove array options

--- bin/bst-test.ts
+++ bin/bst-test.ts
@@ -4,6 +4,12 @@ import {Global} from "../lib/core/global";
 import {BstStatistics, BstCommand} from "../lib/statistics/bst-statistics";
 
 const skillTesting = require("skill-testing-ml");
+const skippedOptions = [
+    "jest.collectCoverageFrom",
+    "jest.moduleFileExtensions",
+    "jest.testPathIgnorePatterns",
+    "jest.testMatch"
+];
 
 program.version(Global.version());
 
@@ -12,10 +18,23 @@ Global.initializeCLI(false).then(() => {
         .usage("[test-pattern-regex]")
         .description("Runs unit-tests for a skill - automatically searches for YML test files and runs them");
 
-    skillTesting.ConfigurationKeys.forEach(element => {
+    const options = skillTesting.ConfigurationKeys.filter(item => skippedOptions.indexOf(item.key) === -1).sort((itemA, itemB) => {
+        if (itemA.key < itemB.key) return -1;
+        if (itemA.key > itemB.key) return 1;
+        return 0;
+    });
+    options.forEach(element => {
         program.option(`--${element.key} <${element.key}>`, element.text);
     });
     program.parse(process.argv);
+
+    // if the third parameter is an option, will be deleted
+    const skillTesterArgs = process.argv.slice(0, 3);
+    if (skillTesterArgs.length >= 3) {
+        if (skillTesterArgs[2].substring(0, 2) === "--") {
+            skillTesterArgs.pop();
+        }
+    }
 
     const configurationOverrides = {};
     skillTesting.ConfigurationKeys.forEach(element => {
@@ -24,7 +43,7 @@ Global.initializeCLI(false).then(() => {
         }
     });
     const testCLI = new skillTesting.CLI();
-    testCLI.run(process.argv, configurationOverrides).then((success) => {
+    testCLI.run(skillTesterArgs, configurationOverrides).then((success) => {
         let nodeId = undefined;
         if (Global.config() && Global.config().secretKey && Global.config().secretKey()) {
             nodeId = Global.config().secretKey();
