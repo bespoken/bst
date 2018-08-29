00e372f4324b48517d82626eac4fef055c3c85c2
ecruzado <edgarivan.cruzado@gmail.com>
2018-08-28 19:56:21 -0500

    order options on test, default parameter

--- bin/bst-test.ts
+++ bin/bst-test.ts
@@ -12,10 +12,23 @@ Global.initializeCLI(false).then(() => {
         .usage("[test-pattern-regex]")
         .description("Runs unit-tests for a skill - automatically searches for YML test files and runs them");
 
-    skillTesting.ConfigurationKeys.forEach(element => {
+    const options = skillTesting.ConfigurationKeys.sort((itemA, itemB) => {
+        if (itemA.key < itemB.key) return -1;
+        if (itemA.key > itemB.key) return 1;
+        return 0;
+    });
+    options.forEach(element => {
         program.option(`--${element.key} <${element.key}>`, element.text);
     });
     program.parse(process.argv);
+
+    // if the third parameter is an option, will be deleted
+    const skillTesterArgs = process.argv.slice(0, 3);
+    if (skillTesterArgs.length >= 3) {
+        if (skillTesterArgs[2].substring(0, 2) === "--") {
+            skillTesterArgs.pop();
+        }
+    }
 
     const configurationOverrides = {};
     skillTesting.ConfigurationKeys.forEach(element => {
@@ -24,7 +37,7 @@ Global.initializeCLI(false).then(() => {
         }
     });
     const testCLI = new skillTesting.CLI();
-    testCLI.run(process.argv, configurationOverrides).then((success) => {
+    testCLI.run(skillTesterArgs, configurationOverrides).then((success) => {
         let nodeId = undefined;
         if (Global.config() && Global.config().secretKey && Global.config().secretKey()) {
             nodeId = Global.config().secretKey();
