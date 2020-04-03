68821461aededd1e464f83a44bc8fdd5ac5bc87e
Juan Perata <juan@bespoken.io>
2020-04-03 17:01:40 -0500

    Add support for HTTPS_PROXY, fix typescript issues (#641)

--- test/external/source-name-generator-test.ts
+++ test/external/source-name-generator-test.ts
@@ -52,4 +52,48 @@ describe("Source Name Generator", function() {
         };
         assert.deepEqual(response, expectedResponse);
     });
+
+    it("Creates a new source with a proxy", async function() {
+        process.env.HTTPS_PROXY = "http://127.0.0.1:1234";
+        const sourceNameGenerator = new SourceNameGenerator();
+        const response = await sourceNameGenerator.createDashboardSource("id", "secretKey");
+        const expectedResponse = {
+            uri: "https://source-api.bespoken.tools/v1/createSource",
+            headers: {
+            },
+            body: {
+                source: {
+                    id: "id",
+                    secretKey: "secretKey",
+                    name: "id",
+                    liveDebug: true,
+                },
+            },
+            json: true,
+            timeout: 30000,
+            "agent": {
+                "_events": {},
+                "_eventsCount": 0,
+                "maxFreeSockets": 1,
+                "maxSockets": 1,
+                "proxy": {
+                    "host": "127.0.0.1",
+                    "hostname": "127.0.0.1",
+                    "href": "http://127.0.0.1:1234/",
+                    "port": 1234,
+                    "protocol": "http:",
+                    "slashes": true,
+                },
+                "requests": {},
+                "secureProxy": false,
+                "sockets": {},
+            }
+        };
+        assert.deepEqual(response.body, expectedResponse.body);
+        assert.equal(response.uri, expectedResponse.uri);
+        assert.equal(response.agent.proxy.host, expectedResponse.agent.proxy.host);
+        assert.equal(response.agent.proxy.port, expectedResponse.agent.proxy.port);
+
+        process.env.HTTPS_PROXY = undefined;
+    });
 });
--- test/external/spokes-test.ts
+++ test/external/spokes-test.ts
@@ -100,5 +100,70 @@ describe("Spokes", function() {
 
            assert.deepEqual(response, expectedResponse);
        });
+
+       it("with values from constructor using proxy", async function() {
+           process.env.HTTPS_PROXY = "http://127.0.0.1:1234";
+
+           mockery.registerMock("request-promise-native", {
+               post: function(options) {
+                   return options;
+               }
+           });
+           const SpokesClient = require("../../lib/external/spokes").SpokesClient;
+
+           const spokes = new SpokesClient("id", "secretKey");
+           const response = await spokes.createPipe();
+           const expectedResponse = {
+               uri: "https://api.bespoken.link/pipe",
+               headers: {
+                   "x-access-token": "4772616365-46696f72656c6c61"
+               },
+               body: {
+                   uuid: "secretKey",
+                   diagnosticsKey: null,
+                   endPoint: {
+                       name: "id"
+                   },
+                   http: {
+                       url: "https://proxy.bespoken.tools",
+                   },
+                   path: "/",
+                   pipeType: "HTTP",
+                   proxy: true
+               },
+               json: true,
+               timeout: 30000,
+               // We add the endpoint on actual call to createPipe
+               endPoint: {
+                   name: "id"
+               },
+               "agent": {
+                   "_events": {},
+                   "_eventsCount": 0,
+                   "maxFreeSockets": 1,
+                   "maxSockets": 1,
+                   "proxy": {
+                       "host": "127.0.0.1",
+                       "hostname": "127.0.0.1",
+                       "href": "http://127.0.0.1:1234/",
+                       "port": 1234,
+                       "protocol": "http:",
+                       "slashes": true,
+                   },
+                   "requests": {},
+                   "secureProxy": false,
+                   "sockets": {},
+               }
+           };
+
+           assert.deepEqual(response.body, expectedResponse.body);
+           assert.equal(response.uri, expectedResponse.uri);
+           assert.deepEqual(response.headers, expectedResponse.headers);
+           assert.deepEqual(response.endPoint, expectedResponse.endPoint);
+           assert.equal(response.agent.proxy.host, expectedResponse.agent.proxy.host);
+           assert.equal(response.agent.proxy.port, expectedResponse.agent.proxy.port);
+
+           process.env.HTTP_PROXY = undefined;
+       });
     });
 });
