d37065212dde02dbea4f8b27445ee56cd5d0e741
Maximo Rosales Diaz <max@bespoken.io>
2022-01-12 08:58:09 -0500

    FIX: Change the values of the files generated by bst init when type e2e and multi locale.

--- test/init/init-util-test.ts
+++ test/init/init-util-test.ts
@@ -1,8 +1,11 @@
 import * as assert from "assert";
-import {InitUtil} from "../../lib/init/init-util";
+import { InitUtil } from "../../lib/init/init-util";
 import * as fs from "fs";
+import { get } from "lodash";
+import { TestParser } from "skill-testing-ml";
+import { Global } from "../../lib/core/global";
 
-describe("init util", function() {
+describe("init util", function () {
     beforeEach(() => {
         if (!fs.existsSync("test/init/temp")) {
             fs.mkdirSync("test/init/temp");
@@ -98,9 +101,122 @@ describe("init util", function() {
         });
     });
 
+    describe("init with multi locale and e2e", () => {
+        it("Should create only invocationUtterance and helpUtterance", async () => {
+            const locales = ["en-US", "es-PE"];
+            const projectName = "hello world";
+            await new InitUtil("e2e", "alexa", "index.js", locales.join(","), projectName, undefined, undefined, false).createFiles();
+
+            const existUnitTestFile = "test/unit/index.test.yml";
+            const existE2eTestFile = "test/e2e/index.e2e.yml";
+            const existTestingFile = "testing.json";
+
+
+            assert.ok(!fs.existsSync(existUnitTestFile));
+            assert.ok(fs.existsSync(existE2eTestFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            assert.ok(fs.existsSync(existTestingFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            // validate files were create
+            assert.ok(locales.map(f => fs.existsSync(`test/e2e/locales/${f}.yml`)).filter(v => v === false).length === 0);
+
+            await Global.loadConfig();
+            const parser: TestParser = new TestParser("test/e2e/index.e2e.yml");
+            const testSuite = parser.parse({});
+            await testSuite.loadLocalizedValues();
+
+            assert.deepStrictEqual(testSuite.tests.map(t => t.interactions).reduce((p, c) => p.concat(c), []).map(v => get(v, "utterance")), ["invocationUtterance", "$HELP_UTTERANCE"]);
+        });
+
+        it("Should create only invocationUtterance and helpUtterance in locales", async () => {
+            const locales = ["en-US", "es-PE"];
+            const projectName = "hello world";
+            await new InitUtil("e2e", "alexa", "index.js", locales.join(","), projectName, undefined, undefined, false).createFiles();
+
+            const existUnitTestFile = "test/unit/index.test.yml";
+            const existE2eTestFile = "test/e2e/index.e2e.yml";
+            const existTestingFile = "testing.json";
+
+
+            assert.ok(!fs.existsSync(existUnitTestFile));
+            assert.ok(fs.existsSync(existE2eTestFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            assert.ok(fs.existsSync(existTestingFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            // validate files were create
+            assert.ok(locales.map(f => fs.existsSync(`test/e2e/locales/${f}.yml`)).filter(v => v === false).length === 0);
+
+            await Global.loadConfig();
+            const parser: TestParser = new TestParser("test/e2e/index.e2e.yml");
+            const testSuite = parser.parse({});
+            await testSuite.loadLocalizedValues();
+
+            assert.deepStrictEqual(get(testSuite, `localizedValues[${locales[0]}].invocationUtterance`, null), `Open ${projectName} overview`);
+            assert.deepStrictEqual(get(testSuite, `localizedValues[${locales[0]}].$HELP_UTTERANCE`, null), "help");
+        });
+
+        it("Should not create locale files if only one locale", async () => {
+            const locales = ["en-US"];
+            const projectName = "hello world";
+            await new InitUtil("e2e", "alexa", "index.js", locales.join(","), projectName, undefined, undefined, false).createFiles();
+
+            const existUnitTestFile = "test/unit/index.test.yml";
+            const existE2eTestFile = "test/e2e/index.e2e.yml";
+            const existTestingFile = "testing.json";
+
+
+            assert.ok(!fs.existsSync(existUnitTestFile));
+            assert.ok(fs.existsSync(existE2eTestFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            assert.ok(fs.existsSync(existTestingFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            // validate files were create
+            assert.ok(locales.map(f => fs.existsSync(`test/e2e/locales/${f}.yml`)).filter(v => v === true).length === 0);
+            await Global.loadConfig();
+            const parser: TestParser = new TestParser("test/e2e/index.e2e.yml");
+            const testSuite = parser.parse({});
+            await testSuite.loadLocalizedValues();
+
+            assert.deepStrictEqual(testSuite.tests.map(t => t.interactions).reduce((p, c) => p.concat(c), []).map(v => get(v, "utterance")), [`open ${projectName}`, "help"]);
+        });
+
+        it("Should not create invocationUtterance and helpUtterance in locales if only one locale", async () => {
+            const locales = ["en-US"];
+            await new InitUtil("e2e", "alexa", "index.js", locales.join(","), "hello world", undefined, undefined, false).createFiles();
+
+            const existUnitTestFile = "test/unit/index.test.yml";
+            const existE2eTestFile = "test/e2e/index.e2e.yml";
+            const existTestingFile = "testing.json";
+
+
+            assert.ok(!fs.existsSync(existUnitTestFile));
+            assert.ok(fs.existsSync(existE2eTestFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            assert.ok(fs.existsSync(existTestingFile));
+            assert.ok(fs.existsSync(existTestingFile));
+
+            // validate files were create
+            assert.ok(locales.map(f => fs.existsSync(`test/e2e/locales/${f}.yml`)).filter(v => v === true).length === 0);
+            await Global.loadConfig();
+            const parser: TestParser = new TestParser("test/e2e/index.e2e.yml");
+            const testSuite = parser.parse({});
+            await testSuite.loadLocalizedValues();
+
+            assert.deepStrictEqual(get(testSuite, `localizedValues[${locales[0]}].invocationUtterance`, null), null);
+            assert.deepStrictEqual(get(testSuite, `localizedValues[${locales[0]}].helpUtterance`, null), null);
+        });
+    });
+
     function deleteFolderRecursive(path: string) {
         if (fs.existsSync(path)) {
-            fs.readdirSync(path).forEach(function(file, index) {
+            fs.readdirSync(path).forEach(function (file, index) {
                 const curPath = path + "/" + file;
                 if (fs.lstatSync(curPath).isDirectory()) { // recurse
                     deleteFolderRecursive(curPath);
