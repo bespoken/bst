0e67337a26867a7fbbc6184d6e2ad064c33790eb
Edgar <edgar@bespoken.io>
2022-02-10 17:50:34 -0500

    update test command

--- bin/bst-test.ts
+++ bin/bst-test.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import {program} from "commander";
-import {Global} from "../lib/core/global";
-import {BstStatistics, BstCommand} from "../lib/statistics/bst-statistics";
+import { program } from "commander";
+import { Global } from "../lib/core/global";
+import { BstStatistics, BstCommand } from "../lib/statistics/bst-statistics";
 
 const skillTesting = require("skill-testing-ml");
 const skippedOptions = [
@@ -15,9 +15,36 @@ program.version(Global.version());
 
 Global.initializeCLI(false).then(() => {
     program
-        .usage("[test-pattern-regex]")
-        .description("Runs unit or end-to-end tests for a skill - automatically searches for YML test files and runs them");
+        .argument("[testPattern]")
+        .description("Runs unit or end-to-end tests for a skill - automatically searches for YML test files and runs them")
+        .action((testPattern: string) => {
+            // the first arguments include the node version and execution path
+            const skillTesterArgs = process.argv.slice(0, 2);
+            if (testPattern) {
+                skillTesterArgs.push(testPattern);
+            }
 
+            const configurationOverrides = {
+                client: "CLI",
+            };
+            const programOptions: any = program.opts();
+            skillTesting.ConfigurationKeys.forEach(element => {
+                if (programOptions[element.key]) {
+                    configurationOverrides[element.key] = programOptions[element.key];
+                }
+            });
+            const testCLI = new skillTesting.CLI();
+            testCLI.run(skillTesterArgs, configurationOverrides).then((success) => {
+                let nodeId = undefined;
+                if (Global.config() && Global.config().secretKey && Global.config().secretKey()) {
+                    nodeId = Global.config().secretKey();
+                }
+                BstStatistics.instance().record(BstCommand.test, undefined, nodeId, Global.version());
+                process.exitCode = success ? 0 : 1;
+            });
+        });
+
+    // dynamically set the options using the configuration from skill testing ml
     const optionsFiltered = skillTesting.ConfigurationKeys.filter(item => skippedOptions.indexOf(item.key) === -1);
     let regultarOptions = [];
     let jestOptions = [];
@@ -42,30 +69,4 @@ Global.initializeCLI(false).then(() => {
     });
     program.parse(process.argv);
 
-    // if the third parameter is an option, will be deleted
-    const skillTesterArgs = process.argv.slice(0, 3);
-    if (skillTesterArgs.length >= 3) {
-        if (skillTesterArgs[2].substring(0, 2) === "--") {
-            skillTesterArgs.pop();
-        }
-    }
-
-    const configurationOverrides = {
-        client: "CLI",
-    };
-    const programOptions: any = program.opts();
-    skillTesting.ConfigurationKeys.forEach(element => {
-        if (programOptions[element.key]) {
-            configurationOverrides[element.key] = programOptions[element.key];
-        }
-    });
-    const testCLI = new skillTesting.CLI();
-    testCLI.run(skillTesterArgs, configurationOverrides).then((success) => {
-        let nodeId = undefined;
-        if (Global.config() && Global.config().secretKey && Global.config().secretKey()) {
-            nodeId = Global.config().secretKey();
-        }
-        BstStatistics.instance().record(BstCommand.test, undefined, nodeId, Global.version());
-        process.exitCode = success ? 0 : 1;
-    });
 });
